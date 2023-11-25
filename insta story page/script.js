826e467d06e4ba45da14fc43fd07287c9e443c03
sibananda Bera <147391872+sibananda67@users.noreply.github.com>
2023-11-25 17:54:08 +0530

    first commit

---
+++ insta story page/script.js
@@ -0,0 +1,32 @@
+var arr = [
+    {dp:"https://images.unsplash.com/photo-1700344207586-7374ef84638a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1699893062191-6ac866d4bf60?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1699730132083-360f1d7a8f83?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://plus.unsplash.com/premium_photo-1681530700755-e8079add58ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMzd8fHxlbnwwfHx8fHw%3D"},
+    {dp:"https://images.unsplash.com/photo-1700235120867-3517dbe5dd52?q=80&w=1922&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1526510747491-58f928ec870f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},    
+    {dp:"https://images.unsplash.com/photo-1645994743395-83d4b809c321?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1623039497026-00af61471107?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1493106819501-66d381c466f1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1504904488824-dbeda3cb530b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+];
+
+
+var storyian = document.querySelector("#storyian")
+var clutter = "" ;
+arr.forEach(function(elem,idx){
+    clutter += ` <div class="story">
+    <img id= " ${idx}" src="${elem.dp}" alt="">
+</div>` ;
+});
+
+
+storyian.innerHTML = clutter;
+
+storyian.addEventListener("click", function (dets){
+    document.querySelector("#full-screen").style.display = "block"
+
+    document.querySelector("full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+
+    setTimeout(function(){
+        document.querySelector("#full-screen").style.display = "none"
+    },3000)
+});
+
+
